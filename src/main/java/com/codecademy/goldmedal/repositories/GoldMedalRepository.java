bae75f3a52efceae5535274de4d8e5e2332aa877
joaoalbertorsc <jaoalbertorsc@gmail.com>
2025-09-18 20:10:19 -0300

    repositories update: add query for total medals count by country, adjust ID type in `GoldMedalRepository`

--- src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
+++ src/main/java/com/codecademy/goldmedal/repositories/GoldMedalRepository.java
@@ -1,11 +1,13 @@
 package com.codecademy.goldmedal.repositories;
 
+import com.codecademy.goldmedal.model.CountryMedalsCount;
 import com.codecademy.goldmedal.model.GoldMedal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface GoldMedalRepository extends JpaRepository<GoldMedal, Integer> {
+public interface GoldMedalRepository extends JpaRepository<GoldMedal, Long> {
     int countByCountryAndGender(String countryName, String women);
     int countBySeason(String winter);
     int countByCountry(String countryName);
@@ -20,4 +22,7 @@ public interface GoldMedalRepository extends JpaRepository<GoldMedal, Integer> {
     List<GoldMedal> getByCountryOrderByNameDesc(String countryName);
     List<GoldMedal> getByCountryOrderByEventAsc(String countryName);
     List<GoldMedal> getByCountryOrderByEventDesc(String countryName);
+
+    @Query("SELECT new com.codecademy.goldmedal.model.CountryMedalsCount(g.country,COUNT(g.country)) FROM GoldMedal g GROUP BY g.country")
+    List<CountryMedalsCount> findTotalMedalsByCountry();
 }
